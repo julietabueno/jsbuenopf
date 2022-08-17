7340969b5030717517aee00fefd00fdaad7592d6
julietabueno <bueno.julieta.ok@gmail.com>
2022-08-17 12:04:44 -0300

    agrego SweetAlert a los botones de comrpa

--- script.js
+++ script.js
@@ -149,60 +149,56 @@ const resultado = document.getElementById("infoUsuarios");
 const realizarCambio = (usuario) => {
     let aux = "";
     aux += `
-    <h2 class="card-title">Hola ${usuario.nombre}</h2>
-
-    <div class="card cardUsuarios" style="width: 18rem;">
-        <div class="card-body">
-            <h6 class="card-subtitle mb-2 ">Tus $${usuario.monto} equivalen a U$S ${dividir(usuario.monto, valorDolar)}</h6>
-        </div>
-    </div>
+    <div class="container-fluid text-center">
+            <div class="col card-body cardUsuarios">
+                <h2 class="card-title">Hola ${usuario.nombre}</h2>
+                <h3 class="card-subtitle py-1">Tus $${usuario.monto} equivalen a U$S ${dividir(usuario.monto, valorDolar)}</h3>
+            </div>
     
-<div class="col-lg-6 col-md-12 d-flex justify-content-evenly">
-                            <div class="row">
-                                <div class="col-md-auto">
-                                    <div class="card cardUsuarios" style="width: 8rem;">
-                                        <img src="img/btcLogo.svg" class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <h2>Bitcoin</h2>
-                                            <p class="cardBtc">BTC ${dividir(usuario.monto, valorBitcoin)}</p>
-                                        </div>
-                                        <div>
-                                        <button id="btnCompraBtc" class="btn btnStyle btn-sm">Comprar</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-auto">
-                                    <div class="card cardUsuarios" style="width: 8rem;">
-                                        <img src="img/ethLogo.svg" class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <h2>Ethereum</h2>
-                                            <p class="cardBtc">ETH ${dividir(usuario.monto, valorEthereum)}</p>
-                                        </div>
-                                        <div>
-                                        <button id="btnCompraEth" class="btn btnStyle btn-sm">Comprar</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-auto">
-                                    <div class="card cardUsuarios" style="width: 8rem;">
-                                        <img src="img/usdcLogo.svg" class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <h2>USDC</h2>
-                                            <p class="cardBtc">USDC ${dividir(usuario.monto, valorUSCD)}</p>
-                                        </div>
-                                        <div>
-                                        <button id="btnCompraUSDC" class="btn btnStyle btn-sm">Comprar</button>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div> 
-                        `
+            <div class="row">
+                <div class="col">
+                    <div class="card cardUsuarios" >
+                        <img src="img/btcLogo.svg" class="card-img-top img-fluid" alt="...">
+                        <div class="card-body">
+                            <h2>Bitcoin</h2>
+                            <h3 class="cardBtc">BTC ${dividir(usuario.monto, valorBitcoin)}</h3>
+                        </div>
+                    <div>
+                        <button id="btnCompraBtc" class="btn btnStyle btn-sm">Comprar</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card cardUsuarios">
+                    <img src="img/ethLogo.svg" class="card-img-top img-fluid" alt="...">
+                    <div class="card-body">
+                        <h2>Ethereum</h2>
+                        <p class="cardBtc">ETH ${dividir(usuario.monto, valorEthereum)}</p>
+                    </div>
+                    <div>
+                        <button id="btnCompraEth" class="btn btnStyle btn-sm">Comprar</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card cardUsuarios">
+                    <img src="img/usdcLogo.svg" class="card-img-top img-fluid" alt="...">
+                    <div class="card-body">
+                        <h2>USDC</h2>
+                        <p class="cardBtc">USDC ${dividir(usuario.monto, valorUSCD)}</p>
+                    </div>
+                    <div>
+                        <button id="btnCompraUSDC" class="btn btnStyle btn-sm">Comprar</button>
+                    </div>
+                </div>
+            </div>
+    </div>`
 
     resultado.innerHTML = aux;
 }
 /* Agrego alertas de SA2 a los botones de comprar de las cards */
+const btnCompraUSDC = document.getElementById("btnCompraUSDC")
+
 btnCompraUSDC.addEventListener("click", () => {
     Swal.fire({
         title: 'Seguimos con la compra?',
@@ -217,40 +213,10 @@ btnCompraUSDC.addEventListener("click", () => {
             Swal.fire(
                 'Felicitaciones!',
                 'Acabas de confirmar tu compra de Cypto USDC',
-                'success'
+                'OK'
             )
         }
     })
 })
 
 
-
-/* 
-//Muestro el localStorage
-const botonIngresos = document.getElementById("botonIngresos");
-const datosIngresos = document.getElementById("datosIngresos");
-
-botonIngresos.addEventListener("click", () => {
-    const usuarios = JSON.parse(localStorage.getItem("Usuario"));
-    let aux = "";
-    usuarios.forEach(usuario => {
-        aux += `
-       <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">Nombre y Apellido</th>
-                <th scope="col">Dolares a cambiar</th>
-                <th scope="col">e-mail</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>${usuario.nombre} ${usuario.apellido}</td>
-                <td>U$D ${usuario.dolaresEquiv()}</td>
-                <td>${usuario.email}</td>
-                </tr>
-            </tbody>
-        </table>`
-    });
-    datosIngresos.innerHTML = aux;
-}); */
